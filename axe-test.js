e099e0ba0edf4795991bd661a46a0e3c5654ba9d
Jia Gill <96542484+jiasidhugill@users.noreply.github.com>
2024-02-22 17:36:35 -0600

    MongoDB functionality added; extraneous files deleted

--- axe-test.js
+++ axe-test.js
@@ -1,7 +1,3 @@
-// const axecore = require("./axe-core/axe.js");
-// console.log(axecore.getRules(["wcag2aa"]));
-
-// source: https://www.npmjs.com/package/@axe-core/webdriverjs
 require("dotenv").config();
 
 
@@ -11,24 +7,29 @@ const chrome = require("selenium-webdriver/chrome");
 const { MongoClient } = require("mongodb");
 
 async function addData(client, newData) {
-    const result = await client.db(process.env.DB_NAME).collection(process.env.COLLECTION_NAME).insertOne(newData);
-    console.log(`New data added with ID ${result.insertedId}`)
+  const result = await client.db(process.env.DB_NAME).collection(process.env.COLLECTION_NAME).insertOne(newData);
+  console.log(`New data added with ID ${result.insertedId}`)
 }
 
 async function main() {
-    const client = new MongoClient(process.env.MONGO_DB_URI);
+  const client = new MongoClient(process.env.MONGO_DB_URI);
 
-    const driver = new Builder()
+  const driver = new Builder()
     .forBrowser("chrome")
     .setChromeOptions(new chrome.Options())
     .build();
-  await driver.get("https://www.noaa.gov/");
-
+  
   try {
+    await client.connect();
+    await driver.get("https://www.noaa.gov/");
     const results = await new AxeBuilder(driver).analyze();
-    addData(client, results);
+    console.log(results);
+
+    await addData(client, JSON.parse("{}"));
   } catch (e) {
-    console.log(e);
+      console.error(e);
+  } finally {
+      await client.close();
   }
 
   await driver.quit();
