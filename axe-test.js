fcd56dbe5e8098ff5821b5150cd8afd75f927b5b
Jia Gill <96542484+jiasidhugill@users.noreply.github.com>
2024-02-22 17:17:11 -0600

    Axe-Core webdriver implemented

--- axe-test.js
+++ axe-test.js
@@ -1,3 +1,37 @@
-const axe = require("axe-core/axe.js");
+// const axecore = require("./axe-core/axe.js");
+// console.log(axecore.getRules(["wcag2aa"]));
 
-axe.getRules(['wcag2aa', 'wcag2a']);
+// source: https://www.npmjs.com/package/@axe-core/webdriverjs
+require("dotenv").config();
+
+
+const { AxeBuilder } = require("@axe-core/webdriverjs");
+const { Builder } = require("selenium-webdriver");
+const chrome = require("selenium-webdriver/chrome");
+const { MongoClient } = require("mongodb");
+
+async function addData(client, newData) {
+    const result = await client.db(process.env.DB_NAME).collection(process.env.COLLECTION_NAME).insertOne(newData);
+    console.log(`New data added with ID ${result.insertedId}`)
+}
+
+async function main() {
+    const client = new MongoClient(process.env.MONGO_DB_URI);
+
+    const driver = new Builder()
+    .forBrowser("chrome")
+    .setChromeOptions(new chrome.Options())
+    .build();
+  await driver.get("https://www.noaa.gov/");
+
+  try {
+    const results = await new AxeBuilder(driver).analyze();
+    addData(client, results);
+  } catch (e) {
+    console.log(e);
+  }
+
+  await driver.quit();
+}
+
+main();
