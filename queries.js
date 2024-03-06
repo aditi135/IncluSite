7792726ac50183bfa5d89c9fc845dc037e5d5647
Jia Gill <96542484+jiasidhugill@users.noreply.github.com>
2024-03-05 22:02:33 -0600

    Calculated violations and incomplete passes of axe-core tests

--- queries.js
+++ queries.js
@@ -7,8 +7,40 @@ async function findWebsiteAxeCore(client, url) {
     return result;
 }
 
-async function findLeastViolations(client) {
-    //
+async function findLeastViolations(client, url) {
+    // currently just prints a summary; can use numbers as wanted later
+    const data = await client.db(process.env.DB_NAME).collection(process.env.COLLECTION_NAME)
+                       .findOne({url : url});
+    var incser = 0;
+    var incmod = 0;
+    var vioser = 0;
+    var viomod = 0;
+    for (var i = 0; i < data.incomplete.length; i++) {
+        if (data.incomplete[i].impact == "serious") {
+            incser++;
+        } else if (data.incomplete[i].impact == "moderate") {
+            incmod++;
+        }
+    }
+    for (var i = 0; i < data.violations.length; i++) {
+        if (data.violations[i].impact == "serious") {
+            vioser++;
+        } else if (data.violations[i].impact == "moderate") {
+            viomod++;
+        }
+    }
+    console.log();
+    console.log("-------------------------------------------");
+    console.log("               TEST RESULTS");
+    console.log("-------------------------------------------");
+    console.log("Passes: " + data.passes.length);
+    console.log("Incompletes: " + data.incomplete.length);
+    console.log("    Moderate: " + incmod);
+    console.log("    Serious: " + incser);
+    console.log("Violations: " + data.violations.length);
+    console.log("    Moderate: " + viomod);
+    console.log("    Serious: " + vioser);
+    console.log("-------------------------------------------");
 }
 
 async function main() {
@@ -16,8 +48,9 @@ async function main() {
     var test_url_axe = "https:\/\/www.noaa.gov\/";
     try {
         await client.connect();
-        const result = await findWebsiteAxeCore(client, test_url_axe);
-        console.log(result);
+        // const result = await findWebsiteAxeCore(client, test_url_axe);
+        // console.log(result);
+        await findLeastViolations(client, test_url_axe);
     } catch (e) {
         console.error(e);
     } finally {
