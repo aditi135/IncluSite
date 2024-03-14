44db99c27356a25a1bb2c29c1751cc4cadc87e59
Jia Gill <96542484+jiasidhugill@users.noreply.github.com>
2024-03-13 20:48:29 -0700

    added for loop for data cleaning

--- axe-format.js
+++ axe-format.js
@@ -5,48 +5,65 @@ const { MongoClient } = require("mongodb");
 
 async function addData(client, newData) {
     const result = await client.db(process.env.DB_NAME).collection(process.env.COLLECTION_NAME_AXE_UPDATED).insertOne(newData);
-    console.log(`New data added with ID ${result.insertedId}`)
+    console.log(`New data added with ID ${result.insertedId} with url ${newData.url}`)
 }
 
-async function formatAxeData(client, url) {
+var count = 0;
+async function formatAxeData(client, URL) {
     const data = await client.db(process.env.DB_NAME).collection(process.env.COLLECTION_NAME)
-                       .findOne({url : url});
+                       .findOne({url : {$regex : URL}});
     if (data == null) {
         return null;
     }
     var result = JSON.parse("{}");
-    result.url = url;
+    result.url = URL;
+    result.url_dom = data.url;
     result.passes_count = data.passes.length;
     result.incompletes_count = data.incomplete.length;
     result.violations_count = data.violations.length;
     result.list_of_elems = []
-    for (var i = 0; i < result.passes_count; i++) {
-        // .replaceAll used because JSON doesn't support "-"
-        result[data.passes[i].id.replaceAll("-", "_")] = "pass";
-        result.list_of_elems.push(data.passes[i].id.replaceAll("-", "_"));
-    }
-    for (var i = 0; i < result.incompletes_count; i++) {
-        result[data.incomplete[i].id.replaceAll("-", "_")] = "incomplete";
-        result[data.violations[i].id.replaceAll("-", "_")].type = data.violations[i].impact;
-        result.list_of_elems.push(data.passes[i].id.replaceAll("-", "_"));
-    }
-    for (var i = 0; i < result.violations_count; i++) {
-        result[data.violations[i].id.replaceAll("-", "_")] = "violation";
-        result[data.violations[i].id.replaceAll("-", "_")].type = data.violations[i].impact;
-        result.list_of_elems.push(data.passes[i].id.replaceAll("-", "_"));
+    try {
+        for (var i = 0; i < result.passes_count; i++) {
+            // .replaceAll used because JSON doesn't support "-"
+            result[data.passes[i].id.replaceAll("-", "_")] = "pass";
+            result.list_of_elems.push(data.passes[i].id.replaceAll("-", "_"));
+        }
+        for (var i = 0; i < result.incompletes_count; i++) {
+            // .replaceAll used because JSON doesn't support "-"
+            result[data.incomplete[i].id.replaceAll("-", "_")] = "incomplete";//_" + data.incomplete[i].impact;
+            result.list_of_elems.push(data.passes[i].id.replaceAll("-", "_"));
+        }
+        for (var i = 0; i < result.violations_count; i++) {
+            // .replaceAll used because JSON doesn't support "-"
+            result[data.violations[i].id.replaceAll("-", "_")] = "violation_";//" + data.incomplete[i].impact;
+            result.list_of_elems.push(data.passes[i].id.replaceAll("-", "_"));
+        }
+    } catch (e) {
+        count++;
+        console.log(count);
+        return null;
     }
     return result;
 }
 
 async function main() {
     const client = new MongoClient(process.env.MONGO_DB_URI);
+
+    var webData = await import("/Users/jiagill/Documents/VSCode/IncluSite/CodaAda/all-domains-30-days.json", {
+        assert: { type: "json" },
+    });
+    webData = webData.default;
   
     try {
-        var webData = await import("./CodeAda/all-domains-30-days.json", {
-            assert: { type: "json" },
-        });
-        webData = webData.default;
-        console.log(webData.length);
+        await client.connect();
+        
+        for (let i = 0; i < webData.length; i++) {
+            const data = await formatAxeData(client, webData[i].domain);
+            if (data == null) {
+                continue;
+            }
+            await addData(client, data);
+        }
     } catch (e) {
         console.error(e);
     } finally {
