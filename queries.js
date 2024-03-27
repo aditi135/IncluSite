b582dd0032edbee379fa569fefb90ad2fe4cfe8f
Jia Gill <96542484+jiasidhugill@users.noreply.github.com>
2024-03-26 20:09:03 -0500

    filter function v1; general accessibilty prioritized over filters

--- queries.js
+++ queries.js
@@ -13,15 +13,50 @@ async function searchData(client, string) {
     return data.toArray();
 }
 
+async function filterData(search_results, filters) {
+    search_results.sort(function(a, b) {
+        a_score = a.pass_count / (a.pass_count + a.incomplete_count + a.violation_count); // DECISION: prioritize GENERAL or FILTERED accessibility? maybe let them pick?
+        b_score = b.pass_count / (b.pass_count + b.incomplete_count + b.violation_count);
+        for (var i = 0; i < filters.length; i++) {
+            try {
+                if (a[filters[i]] == "pass") {
+                    a_score += 1 / (a.pass_count + a.incomplete_count + a.violation_count);
+                } else if (a[filters[i]] == "violation") {
+                    a_score -= 1 / (a.pass_count + a.incomplete_count + a.violation_count);
+                }
+            } catch {}
+            try {
+                if (b[filters[i]] == "pass") {
+                    b_score += 1 / (b.pass_count + b.incomplete_count + b.violation_count);
+                } else if (b[filters[i]] == "violation") {
+                     b_score -= 1 / (b.pass_count + b.incomplete_count + b.violation_count);
+                }
+            } catch {}
+        }
+        return b_score - a_score;
+    });
+    return search_results;
+}
+
 async function main() {
     const client = new MongoClient(process.env.MONGO_DB_URI);
-    // var test_url_axe = "tools.usps.com";
     var test_query = "usps";
     try {
         await client.connect();
 
-        // var url_data = await findURLData(client, test_url_axe);  
         var query_data = await searchData(client, test_query);  
+
+        console.log(query_data);
+        console.log();
+        console.log();
+        console.log();
+        console.log();
+        console.log();
+        console.log();
+        console.log();
+        query_data = filterData(query_data, ["color_contrast"]);
+
+        console.log(query_data);
 
         // console.log(url_data);
         console.log(query_data);
