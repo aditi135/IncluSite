74907f7aad3e2730b27af926cbf060d464c2c647
Jia Gill <96542484+jiasidhugill@users.noreply.github.com>
2024-03-27 16:38:07 -0500

    fixed TypeError

--- queries.js
+++ queries.js
@@ -1,26 +1,26 @@
 require("dotenv").config();
 const {MongoClient} = require("mongodb");
 
-async function findURLData(client, URL) {
+async function findDataByURL(client, URL) {
     const data = await client.db(process.env.DB_NAME).collection(process.env.COLLECTION_NAME_AXE_UPDATED)
                              .findOne({url : URL});
     return data;
 }
 
 async function searchData(client, string) {
-    const raw_results = await client.db(process.env.DB_NAME).collection(process.env.COLLECTION_NAME_AXE_UPDATED)
+    var data = await client.db(process.env.DB_NAME).collection(process.env.COLLECTION_NAME_AXE_UPDATED)
                              .find({url : {$regex: string}});
-    const search_results = raw_results.toArray();
-    search_results.sort(function(a, b) {
+    data = await data.toArray();
+    data.sort(function(a, b) {
         a_score = a.pass_count / (a.pass_count + a.incomplete_count + a.violation_count); // DECISION: prioritize GENERAL or FILTERED accessibility? maybe let them pick?
         b_score = b.pass_count / (b.pass_count + b.incomplete_count + b.violation_count);
         return b_score - a_score;
     });
-    return search_results;
+    return data;
 }
 
 async function filterData(search_results, filters=[]) {
-    search_results.sort(function(a, b) {
+    search_results.sort((function(a, b) {
         a_score = 0;
         b_score = 0;
         for (var i = 0; i < filters.length; i++) {
@@ -40,7 +40,7 @@ async function filterData(search_results, filters=[]) {
             } catch {}
         }
         return b_score - a_score;
-    });
+    }));
     return search_results;
 }
 
@@ -51,21 +51,8 @@ async function main() {
         await client.connect();
 
         var query_data = await searchData(client, test_query);  
-
-        console.log(query_data);
-        console.log();
-        console.log();
-        console.log();
-        console.log();
-        console.log();
-        console.log();
-        console.log();
-
-        console.log(query_data);
         query_data = filterData(query_data, ["color_contrast"]);
 
-
-        // console.log(url_data);
         console.log(query_data);
     } catch (e) {
         console.error(e);
